c7bb5fee69b681bbe7163c76d6000ca40d37cc9c
Anant Kumar <adwayaanant@gmail.com>
2026-04-27 23:09:25 +0530

    authenticate user

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/connectDB.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import authRouter from "./routes/auth.route.js"
 dotenv.config()
 
 const app = express()
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.use("/api/auth" , authRouter)
+
+
 const port = process.env.PORT || 6000
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!")
-})
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
